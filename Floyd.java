217dccb953ff2dfcc62ea9edee67fa356f4282ac
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-03-07 21:15:23 -0700

    successful input from users

---
+++ Floyd.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+public class Floyd {
+  private int nVertices;
+  private int[] vertexWeights;
+  private Vertex[][] weightTable;
+  private Scanner kb = new Scanner(System.in);
+
+  protected class Vertex {
+    private double weight;
+    private int nextVertex;
+    private boolean infinity;
+
+    /**
+    * Returns value of weight
+    * @return
+    */
+    public double getWeight() {
+      return weight;
+    }
+
+    /**
+    * Sets new value of weight
+    * @param
+    */
+    public void setWeight(double weight) {
+      this.weight = weight;
+    }
+
+    /**
+    * Returns value of nextVertex
+    * @return
+    */
+    public int getNextVertex() {
+      return nextVertex;
+    }
+
+    /**
+    * Sets new value of nextVertex
+    * @param
+    */
+    public void setNextVertex(int nextVertex) {
+      this.nextVertex = nextVertex;
+    }
+
+    /**
+    * Returns value of infinity
+    * @return
+    */
+    public boolean isInfinity() {
+      return infinity;
+    }
+
+    /**
+    * Sets new value of infinity
+    * @param
+    */
+    public void setInfinity(boolean infinity) {
+      this.infinity = infinity;
+    }
+  }
+
+  protected void getVerticeNumber() {
+    System.out.print("Enter number of vertices: ");
+    nVertices = kb.nextInt();
+    weightTable = new Vertex[nVertices][nVertices];
+  }
+
+  protected void getVertexWeights () {
+    for (int i = 0; i < nVertices; i++) {
+      for (int j = 0; j < nVertices; j++) {
+          System.out.print("Enter weight for (" +
+            (i + 1) + ", " + (j + 1) + ")\n" +
+            "(or -1 for no edge): ");
+          double weight = kb.nextDouble();
+          Vertex vertex = new Vertex();
+        if (weight != -1) {
+          vertex.setInfinity(false);
+          vertex.setWeight(weight);
+          vertex.setNextVertex(j);
+        } else {
+          vertex.setInfinity(true);
+          vertex.setWeight(-1);
+          vertex.setNextVertex(-1);
+        }
+        weightTable[i][j] = vertex;
+      }
+    }
+    printInitialTable();
+  }
+
+  protected void printInitialTable() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < nVertices; i++) {
+      for (int j = 0; j < nVertices; j++) {
+        if(weightTable[i][j].isInfinity()) {
+          sb.append("-");
+        } else {
+          sb.append(weightTable[i][j].getWeight() + "\t");
+        }
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb.toString());
+  }
+
+  /*
+   * Interact with user to get directed graph data.
+   */
+  protected void userInteraction() {
+    getVerticeNumber();
+    getVertexWeights();
+  }
+
+  public static void main(String[] args) {
+    Floyd driver = new Floyd();
+    driver.userInteraction();
+  }
+}
